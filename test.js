5f21efd68cdaa498c907cb490635fcd07aa7d8bd
PujanZZ <pujan2001@gmail.com>
2022-01-16 22:17:16 +0530

    functions are now added

--- test.js
+++ test.js
@@ -1,3 +1,22 @@
 console.log("Test")
 
 console.log("Test 2")
+
+
+// one parameter
+
+function testP(name) {
+    console.log('THIS IS A TEST FOR ' + name)
+}
+testP('me')
+testP('you')
+
+
+// two parameters 
+
+function testO(name,age) {
+    console.log('Hi ' +name + ' you are ' + age + ' years old')
+}
+
+testO('s1',20)
+testO('zy',19)
